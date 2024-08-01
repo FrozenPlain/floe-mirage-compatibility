32f62992e8b8d00683bc6014d0c7d312115b6736
Sam Windell <info@frozenplain.com>
2024-08-01 10:23:12 +0100

    init

---
+++ mirage-compatibility.floe.lua
@@ -0,0 +1,37 @@
+local library = floe.new_library({
+    name = "Mirage Compatibility",
+    tagline = "Files for Mirage compatibility",
+    author = "FrozenPlain",
+    url = "https://github.com/Floe-Synth/Floe-Mirage-Compat",
+    minor_version = 1,
+})
+
+local irs = {
+    "2s Airy 1",
+    "2s Rough Crackle",
+    "3s Creaky Door 2",
+    "3s Crunchy",
+    "3s Rattle",
+    "3s Shivering Cold",
+    "3s Smooooth",
+    "4s Space Didgeridoo",
+    "4s Standard Bright",
+    "4s Wind",
+    "5s Shimmer",
+    "Ambi 4 Fade",
+    "Formant 1",
+    "Realistic Cathedral A",
+    "Realistic Cathedral B",
+    "Realistic Large A",
+    "Realistic Large B",
+    "Realistic Subtle",
+}
+
+for _, ir in ipairs(irs) do
+    floe.add_ir(library, {
+        name = ir,
+        path = "irs/" .. ir .. ".flac",
+    })
+end
+
+return library
